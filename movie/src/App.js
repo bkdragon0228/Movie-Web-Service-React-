8fc86d2342ea976c0682e3fa23c1becfd85092fa
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-05 22:41:09 +0900

    기능 : 리액트 라우터 돔 추가
    리액트 라우터 돔으로 페이지를 분리했다.

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import Movie from './component/Movie';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
 function App() {
-    return null;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movie" element={<Detail />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
