6479b961f098458c5d2f0b998c293e9666c006cd
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-05 22:29:49 +0900

    리팩토링 : 라우트 분리하기
    원래 app 컴포넌트의 내용들을 home이라는 라우트를 만들어 옮기고
    detail 파일을 생성만 했다.

--- movie/src/App.js
+++ movie/src/App.js
@@ -3,38 +3,7 @@ import { useState, useEffect } from 'react';
 import Movie from './component/Movie';
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-
-    const API_URL = 'https://api.themoviedb.org/3';
-    const API_KEY = 'a5a1eb270fa002cab6c71bb37180961c';
-
-    const fetchMovie = async () => {
-        const responce = await fetch(
-            `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-        );
-        const json = await responce.json();
-        setMovies([...json.results]);
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        fetchMovie();
-    }, []);
-
-    return (
-        <div className="App">
-            {loading ? (
-                <h1>Loading...</h1>
-            ) : (
-                <div>
-                    {movies.map((movie) => (
-                        <Movie movieArr={movie} key={movie.id} />
-                    ))}
-                </div>
-            )}
-        </div>
-    );
+    return null;
 }
 
 export default App;
