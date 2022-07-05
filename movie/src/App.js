0094c5e0dd2a73861d420781bdc6c4a4d7aa5499
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-05 22:02:45 +0900

    기능 : 영화 이미지, 이름, 설명 추가보이기
    api로 받아온 영화 정보를 볼 수 있게 추가했다.
    api를 바꿨다 이전것은 정보가 적어서 더 많은 정보를 주는 것으로 했다.

--- movie/src/App.js
+++ movie/src/App.js
@@ -5,21 +5,43 @@ function App() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
 
+    const API_URL = 'https://api.themoviedb.org/3';
+    const API_KEY = 'a5a1eb270fa002cab6c71bb37180961c';
+    const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
     const fetchMovie = async () => {
         const responce = await fetch(
-            'http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=f5eef3421c602c6cb7ea224104795888'
+            `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
         );
-        const responceJson = await responce.json();
-        console.log(responceJson);
-        setMovies(responceJson.movieListResult.movieList);
+        const json = await responce.json();
+        setMovies([...json.results]);
         setLoading(false);
-        return responceJson;
     };
+
     useEffect(() => {
         fetchMovie();
     }, []);
 
-    return <div className="App">{loading ? <h1>Loading...</h1> : null}</div>;
+    return (
+        <div className="App">
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <div>
+                    {movies.map((movie) => (
+                        <div key={movie.id}>
+                            <img
+                                src={`${IMAGE_BASE_URL}w500/${movie.backdrop_path}`}
+                            />
+                            <h2>{movie.title}</h2>
+                            <p>{movie.overview}</p>
+                            {/* <ul>{}</ul> */}
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
